903c158f8f9aefbdb44d7a1dd23e57a27a75f03c
Ahmed-Abdo1 <161593245+Ahmed-Abdo1@users.noreply.github.com>
2024-11-29 14:03:43 +0200

    Add files via upload

---
+++ final_project-main/analytica/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+from flask_login import LoginManager
+from flask_mail import Mail
+from config import Config
+
+from pymongo.mongo_client import MongoClient
+
+
+import os
+
+app = Flask(__name__)
+uri = "mongodb+srv://ahmedshawaly70:UTf9WbkuXnxT7P4N@cluster0.pvvq2vu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+app.config.from_object(Config)
+
+ 
+
+
+cwd = os.getcwd()
+with open(os.path.join(cwd, 'analytica/stopwords.txt'), 'r') as file:
+    stopwords = [line.strip() for line in file.readlines()]
+
+
+
+db = SQLAlchemy(app)
+mail = Mail(app)
+bycrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+def init_mongo():
+    client = MongoClient(uri)
+    try:
+        client.admin.command('ping')
+        print("Connected to MongoDB!")
+        return client
+    except Exception as e:
+        print(f"Failed to connect to MongoDB: {e}")
+        return None
+
+mongo_client = init_mongo()
+
+
+
+
+
+
+
+
+from analytica import routes
